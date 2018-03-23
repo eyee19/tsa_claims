d589d7c8e7020019d4442e2d3d4935af68d0e239
Everett Yee <eyee305@gmail.com>
2018-03-22 20:29:33 -0700

    testing data frame that follows certain condition

--- tsa_project.R
+++ tsa_project.R
@@ -9,13 +9,17 @@ dat2 <- na.omit(dat) #removing any rows with NA values
 
 names(dat2)
 
-finalDat <- dat2[c(4:12)] #selecting relevant variables
+finalDat <- dat2[c(5:12)] #selecting relevant variables
 names(finalDat)
-
+summary(finalDat)
+table(finalDat$Airport.Code)
+length(unique(finalDat[["Airport.Code"]]))
 barplot(table(finalDat$Airport.Code))
 barplot(table(finalDat$Claim.Type))
 barplot(table(finalDat$Item))
 
-#random forest
-#support vector machine
+plot(finalDat$Claim.Amount)
+mean(finalDat$Claim.Amount)
+
+test <- finalDat[(finalDat$Airport.Name == "Newark International Airport"),] 
 
