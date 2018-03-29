bafc43a9951578c5fd5bf82ca765a38118db5288
Everett Yee <eyee305@gmail.com>
2018-03-29 12:02:33 -0700

    simple plots and more testing

--- tsa_project.R
+++ tsa_project.R
@@ -12,14 +12,22 @@ names(dat2)
 finalDat <- dat2[c(5:12)] #selecting relevant variables
 names(finalDat)
 summary(finalDat)
-table(finalDat$Airport.Code)
-length(unique(finalDat[["Airport.Code"]]))
-barplot(table(finalDat$Airport.Code))
+as.data.frame(table(finalDat$Airport.Name)) #486 entries in the airport names column
+length(unique(finalDat[["Airport.Name"]])) #however this shows that there are only 411 unique names
+
+finalDat$Claim.Amount <- as.numeric(finalDat$Claim.Amount)
+finalDat$Close.Amount <- as.numeric(finalDat$Close.Amount)
+summary(finalDat$Claim.Amount[which(finalDat$Status == "Denied")])
+summary(finalDat$Close.Amount[which(finalDat$Status == "Denied")])
+
+counts <- table(finalDat$Airport.Name)
+barplot(counts, horiz=TRUE, las=1)
+
 barplot(table(finalDat$Claim.Type))
 barplot(table(finalDat$Item))
 
 plot(finalDat$Claim.Amount)
 mean(finalDat$Claim.Amount)
 
-test <- finalDat[(finalDat$Airport.Name == "Newark International Airport"),] 
+test <- finalDat[(finalDat$Airport.Name == "Newark International Airport" | finalDat$Airport.Name == "William P. Hobby"),] 
 
